33296ebc0c0c86ce4bca4769d9e5549d52df5b79
nnnnadia <tristaonadia@gmail.com>
2022-10-25 16:51:11 -0300

    Requisito 7: model `Category` created

---
+++ src/models/Category.js
@@ -0,0 +1,7 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+  });
+  return Category;
+};
