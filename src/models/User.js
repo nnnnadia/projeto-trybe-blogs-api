9e0b22cc66196512ef3eb7a7bbf470ac5ceea554
nnnnadia <tristaonadia@gmail.com>
2022-10-25 16:47:27 -0300

    Requisito 2: model `User` created

---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'users',
+    underscored: true,
+    timestamps: false,
+  });
+  return User;
+};
