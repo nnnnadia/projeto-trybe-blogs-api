267992adfdb4a01f1b37bbbdf9cd861381591a6f
nnnnadia <tristaonadia@gmail.com>
2022-10-26 14:09:57 -0300

    Requisito 2: feat: User's id is now marked as PK on its Model

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: DataTypes.INTEGER,
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
