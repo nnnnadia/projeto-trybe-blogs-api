f28d61f430744771bad693909263929f2e32d282
nnnnadia <tristaonadia@gmail.com>
2022-10-27 19:02:05 -0300

    Requisito 4: feat: create jwt utils' `validateToken`

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -9,6 +9,9 @@ const jwtConfig = {
 
 const createToken = (userId) => jwt.sign({ userId }, secretKey, jwtConfig);
 
+const validateToken = (token) => jwt.verify(token, secretKey);
+
 module.exports = {
   createToken,
+  validateToken,
 };
