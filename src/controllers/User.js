8d5c5cec305022611a99887f7e15ee4655012ccc
nnnnadia <tristaonadia@gmail.com>
2022-10-28 12:36:13 -0300

    refactor: `login` on UserController

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -2,9 +2,9 @@ const { UserService } = require('../services');
 
 const login = async (req, res, next) => {
   const userData = req.body;
-  const response = await UserService.checkLogin(userData);
-  if (!response.type) return res.status(200).json({ token: response.token });
-  next(response);
+  const { type, token, message } = await UserService.checkLogin(userData);
+  if (!type) return res.status(200).json({ token });
+  next({ type, message });
 };
 
 const createUser = async (req, res, next) => {
