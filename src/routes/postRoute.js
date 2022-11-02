f9adba269008c3ef3fb00b0e7d44c256b1b0835d
nnnnadia <tristaonadia@gmail.com>
2022-11-02 13:42:18 -0300

    Requisito 13: feat: route /post GET

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.use(tokenValidation);
 router.post('/', blogPostController.createBlogPost);
+router.get('/', blogPostController.getAllBlogPosts);
 
 module.exports = router;
