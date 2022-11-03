335fd746207b883dedd54a20921419a019d50239
nnnnadia <tristaonadia@gmail.com>
2022-11-03 15:07:25 -0300

    Requisito 15: feat: route /post/:id PUT

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -8,5 +8,6 @@ router.use(tokenValidation);
 router.post('/', blogPostController.createBlogPost);
 router.get('/', blogPostController.getBlogPosts);
 router.get('/:id', blogPostController.getBlogPosts);
+router.put('/:id', blogPostController.updateBlogPost, blogPostController.getBlogPosts);
 
 module.exports = router;
