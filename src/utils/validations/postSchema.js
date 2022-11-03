05ddf2ea370facbb2692808d845f4b702abf9194
nnnnadia <tristaonadia@gmail.com>
2022-11-03 15:01:23 -0300

    Requisito 15: feat: update post Schema

--- src/utils/validations/postSchema.js
+++ src/utils/validations/postSchema.js
@@ -3,5 +3,5 @@ const Joi = require('joi');
 module.exports = Joi.object({
   title: Joi.string().required(),
   content: Joi.string().required(),
-  categoryIds: Joi.array().items(Joi.number()).required(),
+  categoryIds: Joi.array().items(Joi.number()),
 });
