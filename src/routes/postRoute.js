3bad3325df6528086c46fbb7f172a61d52afb8ba
nnnnadia <tristaonadia@gmail.com>
2022-11-10 20:50:17 -0300

    Requisito 16: feat: route /post/:id DELETE

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -9,5 +9,6 @@ router.post('/', blogPostController.createBlogPost);
 router.get('/', blogPostController.getBlogPosts);
 router.get('/:id', blogPostController.getBlogPosts);
 router.put('/:id', blogPostController.updateBlogPost, blogPostController.getBlogPosts);
+router.delete('/:id', blogPostController.deleteBlogPost);
 
 module.exports = router;
